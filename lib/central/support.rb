0ab72905200f773b35e4ccd0b0baab75f34e639f
Fabio Akita <fabioakita@gmail.com>
2016-09-19 15:40:29 -0300

    Moving Mattermost integration from Central to the library

--- lib/central/support.rb
+++ lib/central/support.rb
@@ -1,8 +1,10 @@
 require "central/support/version"
 require "central/support/iteration_service"
 require "central/support/iteration"
+require "central/support/mattermost"
 
 # Ccompatibility mode for drop-in replacement into Central
 # TODO: must remove this after the migration from Central is complete
 IterationService = Central::Support::IterationService
 Iteration = Central::Support::Iteration
+Mattermost = Central::Support::Mattermost
